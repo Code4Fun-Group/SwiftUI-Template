8b1d5d9d62e2324eac155f94b832034742ea9c06
Max Nguyen <max.nguyen@techvify-dn.com>
2023-06-30 16:19:43 +0700

    Update project template

--- Modules/Common/Sources/Common/Classes/Assembly/IApplicationTransitionCoordinator.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-public protocol IApplicationTransitionCoordinator {
-	func navigate(_ transitionType: TransitionType, animated: Bool) throws
-}
--- Modules/Common/Tests/CommonTests/CommonTests.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import Common
-
-final class CommonTests: XCTestCase {
-
-}
